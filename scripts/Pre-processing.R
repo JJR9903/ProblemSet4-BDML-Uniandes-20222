19f7da2001d667fea418b8ff6e9af62ec7f39f0d
Juan José Rincón <rinconbrijj@hotmail.com>
2022-12-02 20:22:02 -0500

    lematizacion

--- scripts/Pre-processing.R
+++ scripts/Pre-processing.R
@@ -23,7 +23,9 @@ dir_set <- function(){
 
 dir_set()
 pacman::p_load(tidyverse,rio,glue,hexbin,patchwork,vip,ggrepel,stringi,tidytext)
-pacman::p_load(stopwords,emoji)
+pacman::p_load(stopwords,emoji,stringi,tm,rvest)
+
+
 
 # importar los datos ------------------------------------------------------
 
@@ -36,37 +38,160 @@ Test<-read.csv(file = file.path("stores/test.csv"))
 
 ### Pre-processing ### ----------------------------------------------------------
 
+stopwords_es <- stopwords::stopwords("es", source = "snowball")
+stopwords_es <- tolower(stopwords_es)
+stopwords_es <- stri_trans_general(str = stopwords_es, id = "Latin-ASCII")
+
+
+# Vamos a crear una función para lematizar (que es lenta como un hpta)
+lematiza = function(frase){
+  print(frase)
+  # Se reemplazan los espacios con +
+  query <- gsub(" ", "+", frase)
+  url_base <- "https://www.lenguaje.com/cgi-bin/lema.exe?edition_field="
+  url_final <- paste0(url_base, query,"&B1=Lematizar")
+  lemma <- read_html(url_final, encoding = "latin1") %>%
+    html_nodes('div') %>% 
+    tail(1) %>% 
+    html_nodes("li") %>% 
+    html_text2() %>% 
+    tail(1)
+  # lemma <- read_html(url_final, encoding = "latin1") %>% 
+  #   html_node(css = "div div div div div li") %>% 
+  #   html_text() 
+  # lemma <- gsub("La palabra:", "", lemma)
+  # lemma <- gsub("tiene los siguientes lemas:", "", lemma)
+  # error <- "\r\n     Palabra no encontrada\r\n     Palabra no encontrada"
+  # lemma <- ifelse(lemma == error, frase, lemma)
+  if (length(lemma) == 0) {
+    return(frase)
+  } else {
+    lemma <- str_split(lemma, "\\n")[[1]][1]
+    return(lemma)
+  }
+}
+
 
 # Normalizar texto --------------------------------------------------------
 
 Train<-Train%>%
-  mutate(text = toupper(text),
+  mutate(text = tolower(text),
          text = stri_trans_general(str=text,id= "Latin-ASCII"),
+         text = gsub(" https.+","",text),
+         text = gsub("https.+","",text),
+         text = removeWords(text, stopwords_es),
+         text = str_replace_all(text, "[^[:alnum:]]", " "),
          text = gsub("[0-9]+","",text),
-         text = gsub("\n"," ",text),
-         text = gsub("[::punct::]","",text),
-         text = gsub(" HTTPS.+","",text),
-         text = gsub("\\s\\s"," ",text),
-         text = gsub("#","",text),
-         emoji_in_text = ifelse(emoji_detect(text, negate = FALSE),1,0),
-         emoji_count = emoji_count(text),
-         emoji = emoji_extract(text),
-         #text = gsub(emoji,"",text),
+         text = gsub("\\s+"," ",text),
+         #emoji_in_text = ifelse(emoji_detect(text, negate = FALSE),1,0),
+         #emoji_count = emoji_count(text),
+         #emoji = emoji_extract(text)
          )
 
-Train$text[1000]
 
-Most_frequent_words <- Train %>%
+
+Train_text <- Train %>%
+  unnest_tokens(output = token, input = text)%>%
+  count(token, sort = TRUE)
+
+diccionario_lemmatizador <- data.frame(corpus = unique(Train_text$token))
+
+diccionario_lemmatizador1<-data.frame(corpus = diccionario_lemmatizador[1:1000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador1[1,2]<-diccionario_lemmatizador1[1,1]
+
+diccionario_lemmatizador2<-data.frame(corpus = diccionario_lemmatizador[1001:2000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador3<-data.frame(corpus = diccionario_lemmatizador[2001:3000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador4<-data.frame(corpus = diccionario_lemmatizador[3001:4000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador4<-data.frame(corpus = diccionario_lemmatizador[4001:5000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador5<-data.frame(corpus = diccionario_lemmatizador[5001:6000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador6<-data.frame(corpus = diccionario_lemmatizador[6001:7000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador7<-data.frame(corpus = diccionario_lemmatizador[7001:8000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador8<-data.frame(corpus = diccionario_lemmatizador[8001:9000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador9<-data.frame(corpus = diccionario_lemmatizador[9001:10000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador10<-data.frame(corpus = diccionario_lemmatizador[10001:11000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador11<-data.frame(corpus = diccionario_lemmatizador[11001:12000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador12<-data.frame(corpus = diccionario_lemmatizador[12001:13000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador13<-data.frame(corpus = diccionario_lemmatizador[13001:14000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador14<-data.frame(corpus = diccionario_lemmatizador[14001:15000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador15<-data.frame(corpus = diccionario_lemmatizador[15001:16000,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador16<-data.frame(corpus = diccionario_lemmatizador[16001:16835,] ) %>%
+  mutate(lemma = sapply(corpus, lematiza))
+
+diccionario_lemmatizador_append<-bind_rows(diccionario_lemmatizador1,diccionario_lemmatizador2,diccionario_lemmatizador3,diccionario_lemmatizador4,
+                                           diccionario_lemmatizador5,diccionario_lemmatizador6,diccionario_lemmatizador7,diccionario_lemmatizador8,
+                                           diccionario_lemmatizador9,diccionario_lemmatizador10,diccionario_lemmatizador11,diccionario_lemmatizador12,
+                                           diccionario_lemmatizador13,diccionario_lemmatizador14,diccionario_lemmatizador15)
+
+saveRDS(diccionario_lemmatizador_append,"stores/diccionario_lemmatizador_append.rds")
+##### frecuencia 
+
+words <- Train %>%
   unnest_tokens(output = word, input = text) %>%
-  anti_join(get_stopwords("es"),"word")%>%
-  count(word, sort = TRUE)
+  filter(!word %in% stopwords_es)%>%
+  filter(!word %in% c("hoy","dia","solo",))
+
+top_words <- words %>%
+  count(word, sort = TRUE) %>%
+  filter(!word %in% as.character(0:10)) %>%
+  slice_max(n, n = 100) %>%
+  pull(word)
+
+
+count_words_ <- Most_frequent_words %>%
+  count(word, name) %>%
+  complete(word, name, fill = list(n = 0)) ## expandir todas las posibles combinaciones
 
 
 count_words <- Train %>%
   unnest_tokens(output = word, input = text) %>%
   anti_join(get_stopwords("es"),"word")%>%
-  count(word, name) %>%
+  count(word, name, sort = TRUE) %>%
   complete(word, name, fill = list(n = 0))
+
+
+word_freqs <- count_words_ %>%
+  group_by(name) %>%
+  mutate(name_sum = sum(n),
+         proportion = n / name_sum) %>%
+  ungroup() %>%
+  filter(word %in% top_words)
+
+
+
+
+
 
 top_words_Lopez<-filter(count_words,name=="Lopez")%>%
   order_by(n)
